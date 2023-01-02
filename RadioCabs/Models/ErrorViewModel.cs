f0af4e10059668a4079c7d21bc467b1ac50091a0
duchuy <dohuyxm123@gmail.com>
2023-01-02 11:37:08 +0700

    Add project files.

---
+++ RadioCabs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RadioCabs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
